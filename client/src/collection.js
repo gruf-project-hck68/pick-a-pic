8c3fecce23f55b719131741f4ec155c030df1229
Geraldioo <bryangeraldioo@gmail.com>
2024-03-19 16:50:24 +0700

    fetch data details

--- client/src/collection.js
+++ client/src/collection.js
@@ -1,17 +1,39 @@
-import { useEffect, useState } from "react"
-import { db } from "../src/firebase"
+import { useEffect, useState } from "react";
+import { db } from "../src/firebase";
 import { onSnapshot, collection } from "firebase/firestore";
 
-
-const [posts, setPosts] = useState([])
+const [posts, setPosts] = useState([]);
 useEffect(() => {
-    onSnapshot(collection(db, "Posts"), (querySnapshot) => {
-        let listPost = [];
-        querySnapshot.forEach((item) => {
-            const post = item.data()
-            post.id = item.id
-            listPost.push(post)
-        })
-        setPosts((prev) => [...listPost])
-    })
-}, [])
+  onSnapshot(collection(db, "Posts"), (querySnapshot) => {
+    let listPost = [];
+    querySnapshot.forEach((item) => {
+      const post = item.data();
+      post.id = item.id;
+      listPost.push(post);
+    });
+    setPosts((prev) => [...listPost]);
+  });
+}, []);
+
+const [details, setDetails] = useState([]);
+const [votes, setVotes] = useState(null);
+useEffect(() => {
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(collection(db, "PostsDetails"));
+    const listDetails = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setDetails(listDetails);
+  };
+
+  fetchData();
+}, []);
+
+useEffect(() => {
+  // Menggunakan kondisi untuk memastikan bahwa detail sudah diambil sebelum memproses votes
+  if (details.length > 0) {
+    const newVotes = details.flatMap((detail) => detail.votes);
+    setVotes(newVotes);
+  }
+}, [details]);
