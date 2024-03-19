241289a6d5ddecec6aea6aa2295b2944d12a1750
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-03-19 16:24:46 +0700

    setup: react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  redirect,
+  Navigate,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"*",
+    element: <Navigate to="/home" replace />,
+  },
+  {
+    path: "/register",
+    element: (
+      <><p>REGISTER</p>
+        {/* <Register /> */}
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <><p>LOGIN</p>
+        {/* <Login /> */}
+      </>
+    ),
+    // loader: () => {
+    //   if (localStorage.access_token) {
+    //     return redirect("/home");
+    //   }
+    //   return null;
+    // },
+  },
+  {
+    element: (
+      <>
+        {/* <Layout /> */}
+      </>
+    ),
+    // loader: () => {
+    //   if (!localStorage.access_token) {
+    //     return redirect("/login");
+    //   }
+    //   return null;
+    // },
+    children: [
+      {
+        path: "/home",
+        element: (
+          <>
+            {/* <Home /> */}
+          </>
+        ),
+      },
+      {
+        path: "/posts",
+        element: (
+          <>
+            {/* <Posts /> */}
+          </>
+        ),
+      },
+      {
+        path: "/my-posts",
+        element: (
+          <>
+            {/* <MyPosts /> */}
+          </>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/logout",
+    loader: () => {
+      localStorage.clear();
+      return redirect("/login");
+    },
+  },
+]);
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
